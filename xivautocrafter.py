f61c477247577e6d458518453e3f341cb1b5b0f4
Kevin Lim <knlim95@gmail.com>
2022-01-19 11:15:31 -0800

    Allow number keys

--- xivautocrafter.py
+++ xivautocrafter.py
@@ -13,7 +13,7 @@ KEY_DELAY = 0.3
 
 POTION_DURATION = 900
 
-KEYS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+KEYS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 SPECIAL_KEYS = {
     "Insert": Key.insert,
     "Delete": Key.delete,
